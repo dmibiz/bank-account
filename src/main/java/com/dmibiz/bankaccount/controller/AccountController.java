f4c9c45ee3b399079fba883c9073857bcfbf4298
Bizjulin <dmitri.bizjulin@cgi.com>
2026-03-03 02:47:29 +0200

    Implement account controller

---
+++ src/main/java/com/dmibiz/bankaccount/controller/AccountController.java
@@ -0,0 +1,52 @@
+package com.dmibiz.bankaccount.controller;
+
+import com.dmibiz.bankaccount.model.Account;
+import com.dmibiz.bankaccount.model.Currency;
+import com.dmibiz.bankaccount.service.AccountService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+
+@Controller
+@RequestMapping("/accounts")
+@RequiredArgsConstructor
+public class AccountController {
+
+    private final AccountService accountService;
+
+    @PostMapping
+    public Account create(@RequestParam String identification) {
+        return accountService.createAccount(identification);
+    }
+
+    @PostMapping("/{id}/credit")
+    public void credit(@PathVariable Long id,
+                       @RequestParam Currency currency,
+                       @RequestParam BigDecimal amount) {
+        accountService.credit(id, currency, amount);
+    }
+
+    @PostMapping("/{id}/debit")
+    public void debit(@PathVariable Long id,
+                      @RequestParam Currency currency,
+                      @RequestParam BigDecimal amount) {
+        accountService.debit(id, currency, amount);
+    }
+
+    @PostMapping("/{id}/exchange")
+    public void exchange(@PathVariable Long id,
+                         @RequestParam Currency from,
+                         @RequestParam Currency to,
+                         @RequestParam BigDecimal amount) {
+        // TODO: implement
+        throw new UnsupportedOperationException("Currency exchange not implemented yet");
+    }
+
+    @GetMapping("/{id}/balance")
+    public BigDecimal balance(@PathVariable Long id,
+                              @RequestParam Currency currency) {
+        return accountService.getBalance(id, currency);
+    }
+}
