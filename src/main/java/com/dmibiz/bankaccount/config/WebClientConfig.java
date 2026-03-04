a1a3f1959c286a5a9cfda36a295c924eaa547f55
Bizjulin <dmitri.bizjulin@cgi.com>
2026-03-04 16:06:49 +0200

    Ignore SSL certificates when calling external logging service for simplicity of solution

--- src/main/java/com/dmibiz/bankaccount/config/WebClientConfig.java
+++ src/main/java/com/dmibiz/bankaccount/config/WebClientConfig.java
@@ -1,15 +1,29 @@
 package com.dmibiz.bankaccount.config;
 
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import javax.net.ssl.SSLException;
+
 
 @Configuration
 public class WebClientConfig {
 
     @Bean
-    public WebClient webClient() {
+    public WebClient webClient() throws SSLException {
+        // This will ignore SSL certificate for simplicity of solution. Not to be used in production.
+        SslContext sslContext = SslContextBuilder.forClient()
+                .trustManager(InsecureTrustManagerFactory.INSTANCE)
+                .build();
+        HttpClient httpClient = HttpClient.create().secure(ssl -> ssl.sslContext(sslContext));
         return WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
                 .baseUrl("https://tools-httpstatus.pickup-services.com")
                 .build();
     }
